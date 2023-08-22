5112eff0fd32bc87f9e04c15c49841c0c4ef9b81
DEOGASS WIDAMBE <87471672+widambeDeograss@users.noreply.github.com>
2023-08-22 14:21:41 +0300

    data

---
+++ src/constants/Colors.ts
@@ -0,0 +1,13 @@
+// interface colorsInterface  {
+//     primary: String,
+//     secondary: String,
+//     accent: String,
+//     addson: String
+// }
+
+export const Colors = {
+    primary:"#a3c733",
+    secondary:"#eef5d6",
+    accent:"#a9cc33",
+    addson:"#f2f8f8"
+}
